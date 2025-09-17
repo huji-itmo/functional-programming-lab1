441889e42fef93378b4891d8bcf0ad60fb2a71ed
huji <huji@mail.com>
2025-09-17 18:39:56 +0300

    feat: add first task

---
+++ Euler9.Test/Tests.fs
@@ -0,0 +1,17 @@
+﻿module Tests
+
+open System
+open Xunit
+open Program
+
+[<Fact>]
+let ``Test triple`` () =
+    Assert.True(checkPythogoreanTriple (3, 4, 5))
+    Assert.False(checkPythogoreanTriple (2, 4, 5))
+
+let ``Test finding triple`` =
+    let result = findTriplet ()
+
+    Assert.True result.IsSome
+
+    Assert.Equal((200, 375, 425), result.Value)
---
+++ Euler9/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+let checkPythogoreanTriple (a: int, b: int, c: int) = a * a + b * b = c * c
+
+let findTriplet () =
+    seq {
+        for a in { 1..1000 } do
+            for b in { a..1000 } do
+                let c = 1000 - a - b
+
+                if a < b && b < c && checkPythogoreanTriple (a, b, c) = true then
+                    yield a, b, c
+    }
+    |> Seq.tryHead
+
+
+
+match findTriplet () with
+| Some(a, b, c) ->
+    printfn $"Pythagorean triplet: {a} {b} {c}"
+    printfn $"Product: {a * b * c}"
+| None -> printfn "No triplet found"
